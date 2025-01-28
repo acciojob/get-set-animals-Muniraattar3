05ecb766332652e8cccbb831b11546234c1778c9
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-01-28 22:44:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species; // Use a private property for species
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species; // Getter for the species property
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`); // Logs the sound of the animal
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof"); // Logs "woof" for a dog
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr"); // Logs "purr" for a cat
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
